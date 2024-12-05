04394bc253b819530c783dbd87290ab10f45f395
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-12-05 20:12:56 +0100

    MOD: Main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
 
   return (
     <>
-      <div className="px-10 lg:py-20 py-2 w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[600px] bg-[url('/images/main-banner.png')] bg-cover bg-center">
+      <section className="px-10 lg:py-20 py-2 w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[600px] bg-[url('/images/main-banner.png')] bg-cover bg-center">
         <h1 className="lg:text-[96px] md:text-[56px] text-[32px] text-white font-bold leading-[110%] lg:mb-10 mb-2">
           Amazing Discounts on Pets Products!
         </h1>
@@ -19,7 +19,13 @@ function Home() {
         >
           Check out
         </button>
-      </div>
+      </section>
+
+      <section className="flex flex-row items-center">
+        <h2 className="heading-2">Categories</h2>
+        <div className="h-[1px] w-full bg-slate-300" />
+        <button className="text-small-grey border-slate-300 border-solid border rounded-md px-2 py-4 mt-20">All categories</button>
+      </section>
     </>
   );
 }
