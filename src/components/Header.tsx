709fe903c57d0af48c6b270393a3b8698dece162
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-12-05 16:10:34 +0100

    ADD: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,49 +1,4 @@
-// import Logo from "../assets/icons/logo.svg?react";
-// import Cart from "../assets/icons/cart.svg?react";
-// import { Link } from "react-router-dom";
-
-// const menuItems = [
-//   { id: 1, label: "Main Page", href: "/" },
-//   { id: 2, label: "Categories", href: "/categories" },
-//   { id: 3, label: "All products", href: "/products" },
-//   { id: 4, label: "All sales", href: "/sales" },
-// ];
-
-// function Header() {
-//   return (
-//     <header className=" flex flex-row justify-between items-center px-10 py-[30px]">
-//       <Link to={"/"}>
-//         <Logo className="transform transition-transform duration-300 hover:scale-125 hover:rotate-45 hover:brightness-125" />
-//       </Link>
-//       <div>
-//         <nav>
-//           <ul className="flex gap-8">
-//             {menuItems.map((item) => (
-//               <li key={item.id}>
-//                 <Link
-//                   to={item.href}
-//                   className="text-txtBlack text-[20px] font-medium leading-[130%] hover:text-hoverBlue"
-//                 >
-//                   {item.label}
-//                 </Link>
-//               </li>
-//             ))}
-//           </ul>
-//         </nav>
-//       </div>
-//       <Link to={"/cart"}>
-//         <Cart className="transform transition-transform duration-300 hover:scale-125 hover:rotate-12 hover:brightness-125" />
-//       </Link>
-//     </header>
-//   );
-// }
-
-// export default Header;
-
-// import { useState } from "react";
 import { Link } from "react-router-dom";
-// import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@shadcn/ui";
-
 import { Menu } from "lucide-react";
 
 import {
@@ -89,7 +44,6 @@ function Header() {
 
       {/* Иконка корзины */}
       <Link to={"/cart"}>
-        {/* <Cart className="hidden md:block transform transition-transform duration-300 hover:scale-125 hover:rotate-12 hover:brightness-125" /> */}
         <Cart className="transform transition-transform duration-300 hover:scale-125 hover:rotate-12 hover:brightness-125" />
       </Link>
 
@@ -97,20 +51,6 @@ function Header() {
       <DropdownMenu>
         <DropdownMenuTrigger className="md:hidden">
           <Menu size={52} />
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8 text-txtBlack"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg> */}
         </DropdownMenuTrigger>
         <DropdownMenuContent align="start" className="w-48 bg-white shadow-lg">
           {menuItems.map((item) => (
